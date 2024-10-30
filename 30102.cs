9e834d89fbff914d65ecaa36ba8eb21b0184b268
alina060509 <165463634+alina060509@users.noreply.github.com>
2024-10-30 17:00:07 +0300

    Add files via upload

---
+++ 30102.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public interface IShare
+{
+    double CalculateArea();
+}
+
+public class Circle : IShare
+{
+    private double _radius;
+    public Circle(double radius)
+    {
+        this._radius = radius;
+    }
+    public double CalculateArea()
+    {
+        return 3.14 * _radius * _radius;
+    }
+}
+
+public class Rectangle: IShare
+{
+    private double len;
+    private double wight;
+
+    public Rectangle(double len, double wight)
+    {
+        this.len = len;
+        this.wight = wight;
+    }
+    public double CalculateArea()
+    {
+        return len * wight;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        IShare circle = new Circle(2);
+        IShare rectangle= new Rectangle(2,3);
+        Console.WriteLine($"площадь круга:{ circle.CalculateArea()}");
+        Console.WriteLine($"площадь прям: {rectangle.CalculateArea()}");
+    }
+}
