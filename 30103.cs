657692a37a8e45ff75d3e25ad3ef7e02efff9bb4
alina060509 <165463634+alina060509@users.noreply.github.com>
2024-10-30 17:00:53 +0300

    Add files via upload

---
+++ 30103.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("животное издает звуууук");
+    }
+}
+class Dog : Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("гав");
+    }
+}
+class Cat : Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("мяу");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        dog.Sound();
+        cat.Sound();
+    }
+}
